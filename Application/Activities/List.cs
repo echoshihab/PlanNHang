a28377e812d90eaa0fe4e9e59d18dcfac28c06a0
shihab <echoshihab@gmail.com>
2020-05-10 20:30:01 -0400

    use mediatr to get list of activities

---
+++ Application/Activities/List.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly ApplicationDbContext _db;
+
+            public Handler(ApplicationDbContext db)
+            {
+                _db = db;
+            }
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activities = await _db.Activities.ToListAsync();
+                return activities;
+            }
+        }
+    }
+}
