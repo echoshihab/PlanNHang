6236e49b9e72a19113418abdbea459dd72c22a3e
shihab <echoshihab@gmail.com>
2020-05-20 12:40:24 -0400

    combine date+time on submit

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -11,6 +11,7 @@ import TextAreaInput from "../../../app/common/form/TextAreaInput";
 import SelectInput from "./SelectInput";
 import { category } from "../../../app/common/options/categoryOptions";
 import DateInput from "../../../app/common/form/DateInput";
+import { combineDateAndTime } from "../../../app/common/util/util";
 
 interface DetailsParams {
   id: string;
@@ -80,7 +81,10 @@ const ActivityForm: React.FC<RouteComponentProps<DetailsParams>> = ({
   // };
 
   const handleFinalFormSubmit = (values: any) => {
-    console.log(values);
+    const dateAndTime = combineDateAndTime(values.date, values.time);
+    const { date, time, ...activity } = values;
+    activity.date = dateAndTime;
+    console.log(activity);
   };
 
   return (
