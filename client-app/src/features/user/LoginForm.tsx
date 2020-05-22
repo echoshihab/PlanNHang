74700114313645e2d583c969876f066be594c94e
shihab <echoshihab@gmail.com>
2020-05-22 19:02:01 -0400

    add login form to client

---
+++ client-app/src/features/user/LoginForm.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Form as FinalForm, Field } from "react-final-form";
+import { Form, Button } from "semantic-ui-react";
+import TextInput from "../../app/common/form/TextInput";
+
+const LoginForm = () => {
+  return (
+    <FinalForm
+      onSubmit={(values) => console.log(values)}
+      render={({ handleSubmit }) => (
+        <Form onSubmit={handleSubmit}>
+          <Field name="email" component={TextInput} placeholder="Email" />
+
+          <Field
+            name="password"
+            component={TextInput}
+            type="password"
+            placeholder="Password"
+          />
+          <Button positive content="Login" />
+        </Form>
+      )}
+    />
+  );
+};
+
+export default LoginForm;
