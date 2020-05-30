8b4ee3d2dbc7f5d987b74f60efcfe2db3fb248e8
shihab <echoshihab@gmail.com>
2020-05-30 18:50:50 -0400

    add userfollower entity

--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace Persistence
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
+        public DbSet<UserFollowing> Followings { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -34,6 +36,25 @@ namespace Persistence
                 .HasOne(a => a.Activity)
                 .WithMany(a => a.UserActivities)
                 .HasForeignKey(a => a.ActivityId);
+
+
+            builder.Entity<UserFollowing>(b =>
+            {
+                b.HasKey(k => new { k.ObserverId, k.TargetId });
+
+                b.HasOne(o => o.Observer)
+                    .WithMany(f => f.Followings)
+                    .HasForeignKey(o => o.ObserverId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                b.HasOne(o => o.Target)
+                    .WithMany(f => f.Followers)
+                    .HasForeignKey(o => o.TargetId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            });
+
+
         }
     }
 
