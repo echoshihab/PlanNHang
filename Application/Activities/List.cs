cd0a63e9b9f65ba978150c06613135b05cd440ad
shihab <echoshihab@gmail.com>
2020-06-01 07:21:08 -0400

    add paging activity logic to API

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,9 +12,26 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<ActivityDto>> { }
+        public class ActivitiesEnvelope
+        {
+            public List<ActivityDto> Activities { get; set; }
+            public int ActivityCount { get; set; }
+        }
 
-        public class Handler : IRequestHandler<Query, List<ActivityDto>>
+        public class Query : IRequest<ActivitiesEnvelope>
+        {
+            //will be used for paging
+            public Query(int? limit, int? offset)
+            {
+                Limit = limit;
+                Offset = offset;
+
+            }
+            public int? Limit { get; set; }
+            public int? Offset { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
         {
             private readonly ApplicationDbContext _db;
             private readonly IMapper _mapper;
@@ -23,16 +41,26 @@ namespace Application.Activities
                 _mapper = mapper;
                 _db = db;
             }
-            public async Task<List<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<ActivitiesEnvelope> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await _db.Activities.ToListAsync();
+                var queryable = _db.Activities.AsQueryable();
+
+                var activities = await queryable
+                .Skip(request.Offset ?? 0)
+                .Take(request.Limit ?? 3).ToListAsync();
 
                 //eager loading
                 // .Include(x => x.UserActivities)
                 // .ThenInclude(x => x.AppUser)
                 // .ToListAsync();
 
-                return _mapper.Map<List<Activity>, List<ActivityDto>>(activities);
+                return new ActivitiesEnvelope
+                {
+                    Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
+                    ActivityCount = queryable.Count()
+                };
+
+
             }
         }
     }
