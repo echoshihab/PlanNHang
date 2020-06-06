c026a37f38ad1664e75e1ee441680e702be7e35a
shihab <echoshihab@gmail.com>
2020-06-06 10:56:28 -0400

    refreshtoken - complete API side

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,14 +1,27 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Application.User;
 using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Controllers
 {
 
     public class UserController : BaseController
     {
+        private readonly IConfiguration _config;
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
@@ -29,6 +42,43 @@ namespace API.Controllers
             return await Mediator.Send(new CurrentUser.Query());
         }
 
+        [AllowAnonymous]
 
+        [HttpPost("refresh")]
+        public async Task<ActionResult<User>> Refresh(RefreshToken.Query query)
+        //need to validate expired token in order to refresh here, because API will not be able to do it due to expired
+        //status 
+        {
+            var principal = GetPrincipalFromExpiredToken(query.Token);
+            query.UserName = principal.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+            return await Mediator.Send(query);
+        }
+
+        private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true, //still need to validate token
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha512,
+            StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new SecurityTokenException("Invalid Token");
+            }
+
+
+            return principal;
+
+        }
     }
+
+
 }
